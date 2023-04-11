b3af5abceec3117c97ba9070292167e0de38e0cc
Micke80 <37250306+Micke80@users.noreply.github.com>
2023-04-11 10:51:18 +0200

    Update Win 10 22H2 Ent - nb-NO.ps1

--- Trademax/Win 10 22H2 Ent - nb-NO.ps1
+++ Trademax/Win 10 22H2 Ent - nb-NO.ps1
@@ -19,9 +19,9 @@ Import-Module OSD -Force
 $Params = @{
     OSVersion = "Windows 10"
     OSBuild = "22H2"
-    OSEdition = "Ent"
+    OSEdition = "Enterprise"
     OSLanguage = "nb-NO"
-    OSLicense = "Retail"
+    OSLicense = "Volume"
     ZTI = $true
 }
 Start-OSDCloud @Params
